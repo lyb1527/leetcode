7d99b7b15497c12bc0028e7a7664188428dcda90
lyb1527 <yingbo_liu@brown.edu>
2017-07-02 23:38:38 -0400

    more array questions

--- leetcode.py
+++ leetcode.py
@@ -988,45 +988,540 @@ Array
 
 
 # Missing Number
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(len(nums)):
+            if i!=nums[i]:
+                return i
+        return len(nums)
 
+class Solution(object):
+        def missingNumber(self, nums):
+            expected = len(nums) * (len(nums)+1)/2
+            return int(expected - sum(nums))
 
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        s = set(nums)
+        for i in range(0, len(nums) + 1):
+            if i not in s:
+                return i
+        return -1
 
 # two Sum
 
-# Move Zeroer
+# Move Zeroes
+'''
+Given an array nums, move all 0's to the end of array while matianting the
+relative orer of the non-zero element
+
+#do this In-Place without making a copy of the array
+# minimize the total number of operations
+
+https://leetcode.com/articles/move-zeroes/
+
+
+'''
+class Solution(object):
+    def moveZeroes(self, a):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        for j in range(len(a)):
+            if a[j] is not 0:
+                a[i], a[j] = a[j], a[i]
+                i += 1
 
 
 # Two Sum II- input is sorted
 
 
 # Majority Element
+'''
+Array of size n, find the majority element.
+Majority element is the element that appear s more than n/2 times
+'''
+
+# Sotring
+
+#python sort TC: o(nlogn)
+def majorityElement4(self, nums):
+    nums.sort()
+    return nums[len(nums)//2]
+
+# one pass + dictionary
+def majorityElement2(self, nums):
+    dic = {}
+    for num in nums:
+        if num not in dic:
+            dic[num] = 1
+        if dic[num] > len(nums)//2:
+            return num
+        else:
+            dic[num] += 1
+
 
 
 #Rotate array
+'''
+Rotate an array of n elements to the right by k steps
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+
+https://leetcode.com/articles/rotate-array/
+Approach-1: Brute Force
+Rotate all the elements of the array in k steps by the rotating the elements by 1 unit iin each steps
+
+Approach-2: Extra Array
+use an extra array in which we place every element of the array at its correct position.
+i.e. the number at index i in the original array is placed at the index i+k
+then we copy the new array to the origanl one
+
+Approach-3: using Reverse
+TC: O(n)
+SC: O(1)
+
+In this approach, we firstly reverse all the elements of the array. Then, reversing the first k elements followed by reversing the rest n-kn−k elements gives us the required result.
+
+Let n=7n=7 and k=3k=3.
+
+Original List                   : 1 2 3 4 5 6 7
+After reversing all numbers     : 7 6 5 4 3 2 1
+After reversing first k numbers : 5 6 7 4 3 2 1
+After revering last n-k numbers : 5 6 7 1 2 3 4 --> Result
 
 
-# Remove uplicates from sorted array
+
+'''
+# Approach-2
+#TC: O(n)     SC: O(n)
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        this solution works, but it is not very pretty. try to come up with another way to do this more simply
+        """
+
+        nums2 = [0]*len(nums)
+
+        for i in range(0, len(nums)):
+            nums2[(i+k)%len(nums2)] = nums[i]
+
+        for i in range(0, len(nums2)):
+            nums[i] = nums2[i]
 
 
+#Approach-3
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        self.reverse(nums, 0, len(nums) -1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) -1)
+
+
+    def reverse(self, nums, start, end):
+        while start < end:
+            tmp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = tmp
+            start += 1
+            end -= 1
+
+
+# Remove duplicates from sorted array
+'''
+remove duplicates in place such that each element appear only once and return new length
+
+Approach-1: two pointers
+
+TC: O(n)    SC: O(1)
+
+Since array already sorted, keep two pointers i and j, where i is the slower-runner and j is the fast -runner
+
+as long as nums[i] = nums[j], we increment j to skip the duplicate
+
+when we encounter nums[j] != nums[i],  the uplicate run has ened so we must copy its value to nums[i+1, i is then incremented
+and we repeat the process again until j reaches the end of arrays]
+
+https://leetcode.com/articles/remove-duplicates-sorted-array/
+'''
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        l1 = 0
+        for l2 in range(1, len(nums)):
+            if nums[l1] != nums[l2]:
+                l1 += 1
+                nums[l1] = nums[l2]
+        return l1 + 1
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums: return 0
+
+        tail = 0
+        for i in range(1, len(nums)):
+            if nums[tail] != nums[i]:
+                tail += 1
+                nums[tail] = nums[i]
+
+        return tail + 1
 # Remove Element
+'''
+Given an array and a value, remove all instances of that value in place and return new length
 
+DO this in place
+
+https://leetcode.com/articles/remove-element/
+
+
+
+Approach #1 (Two Pointers) [Accepted]
+
+Intuition
+
+Since question asked us to remove all elements of the given value in-place,
+ we have to handle it with O(1)O(1) extra space. How to solve it? We can keep
+  two pointers ii and jj, where ii is the slow-runner while jj is the fast-runner.
+
+Algorithm
+
+When nums[j]nums[j] equals to the given value, skip this element by incrementing jj.
+As long as nums[j] \neq valnums[j]≠val, we copy nums[j]nums[j] to nums[i]nums[i] and
+increment both indexes at the same time. Repeat the process until jj reaches the end of the array and the new length is ii.
+
+TC: O(n)   SC: O(1)
+
+
+'''
+
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        length = 0
+        for temp in nums:
+            if temp != val:
+                nums[length] = temp
+                length += 1
+
+        return length
 
 # Array Partition I
+'''
+Given an array of 2n integers, task is to group ints into n pairs of integers,
+say a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+
+
+#Consider the smallest element x. It should be paired with the next smallest element,
+ #because min(x, anything) = x, and having bigger elements only helps you have a
+ #larger score. Thus, we should pair adjacent elements together in the sorted array.
+'''
+
+
+def arrayPairSum(self, A):
+    return sum(sorted(A)[::2])
+
+
+
 
 # Can Place Flowers
+'''
+Flowerbed in which some of the plots are planted and some not.
+However, flowers cannot be planted in adjacent plots - the would compte for water and both Disappeared
+
+Gveien a flower bed(represented as an array containing 0 and 1, where 0 means empty and 1 means not empty)
+and a number n, return if n new flowers can be planted in it without violating the non-ajacent-flowers range-sum-query-immutable
+
+
+https://leetcode.com/articles/can-place-flowers/
+
+Approach - 1: Single Scan
+
+Find out the extra maximum number of flowers, count, that can be
+planted for the given flowerbed arrangement
+
+we traverse over all the elements of the flowered and find out those elements which are 0, impying an empty
+positin. For every such element, we check if its both ajacent positions are also empty. If so, we can planted
+a flower at the current position without without violating the no-adjacent flowers rule.
+For the first an last element, need NOT to check the previous and next ajacent positions
+
+If the count obtained is greater than or equal to n, the required number of flowers to be planted, we
+can plant n flowers in the empty spaces, otherwise not.
+
+
+
+
+
+
+Approach-2: Optimized
+
+Instead of find the maximum value of count that can be obtaine we can stop the process of checking the
+positions for planting flowers as soon sas count becomes equal to n.
+
+Doing this leads to an optimization of the first approache.
+If count never becomes equal to n, n flowers can NOT be palnted a t the empty positions.
+
+'''
+
+# Approach 1
+
+# Time complexity : O(n)O(n). A single scan of the flowerbedflowerbed array of size nn is done.
+#Space complexity : O(1)O(1). Constant extra space is used.
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        if not flowerbed or len(flowerbed) < n:
+            return False
+
+        count = 0
+
+        for i, flower in enumerate(flowerbed):
+            if flower == 0:
+                if (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0) :
+                    flowerbed[i] = 1
+                    count += 1
+
+        return count >= n
+
+
+
+# Approach-2: optimized check n
+# TC: O(n)    SC:O(1)
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        if n == 0:
+            return True
+        for i in range(len(flowerbed)):
+            if (i != 0 and flowerbed[i-1]) or flowerbed[i] or (i != len(flowerbed) - 1 and flowerbed[i+1]):
+                continue
+            flowerbed[i] = 1
+            n -= 1
+            if n == 0:
+                return True
+        return False
 
 # Contains Duplicate
+'''
+Given an array of integers, find if the array contains any duplicates.
 
+Function return to true if value appears at least twice, and return False if eeveyr element is istince
+
+https://leetcode.com/articles/contains-duplicate/
+
+
+Approach-1: Naive Linear Serach
+
+
+Approach-2: Sorting
+TC: O(nlogn)   SC: O(1)
+
+
+Approach-3: Hash table
+TC: O(n)  SC: O(n)
+Time complexity : O(n)O(n). We do search() and insert() for nn times and each operation takes constant time.
+
+Space complexity : O(n)O(n). The space used by a hash table is linear with the number of elements in it.
+
+
+For certain test cases with not very large nn, the runtime of this method can
+ be slower than Approach #2. The reason is hash table has some overhead in
+ maintaining its property. One should keep in mind that real world performance
+ can be different from what the Big-O notation says. The Big-O notation only tells
+ us that for sufficiently large input, one will be faster than the other.
+ Therefore, when nn is not sufficiently large, an O(n)O(n) algorithm can be slower than an O(n \log n)O(nlogn) algorithm.
+'''
+
+
+#Sort
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        nums.sort()
+
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+
+        return False
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        duplicate = set()
+        for n in nums:
+            if n in duplicate:
+                return True
+            else:
+                duplicate.add(n)
+
+        return False
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen = {}
+        for x in nums:
+            if x in seen:
+                return True
+            else:
+                seen[x] = 1
+        return False
 
 # Search Insert Position
-
-
+'''
+KEY: find the first position >= target
+'''
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # find the first position >= target
+        if not nums or len(nums) == 0:
+            return 0
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
+            mid = start + (end - start)/2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid
+            else:
+                end = mid
+        if nums[start] >= target:
+            return start
+        elif nums[end] >= target:
+            return end
+        else:
+            return end + 1
 
 # Find All Numbers Disappeared in an Array
+'''
+Given an array of ints, some elements appear twice or more
 
+Find all the elements of [1, n] inclusive that o not appear in the array
+
+Could you do it without extra space and in O(n) runtime? You may assume the
+ returned list does not count as extra space.
+
+EX:
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[5,6]
+'''
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        all_nums = set(nums)
+        out = []
+        for num in range(1, len(nums)+1):
+            if not num in all_nums:
+                out.append(num)
+        return out
+
+'''
+For each number i in nums,
+we mark the number that i points as negative.
+Then we filter the list, get all the indexes
+who points to a positive number.
+Since those indexes are not visited.
+
+'''
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # For each number i in nums,
+        # we mark the number that i points as negative.
+        # Then we filter the list, get all the indexes
+        # who points to a positive number
+        for i in xrange(len(nums)):
+            index = abs(nums[i]) - 1
+            nums[index] = - abs(nums[index])
+
+        return [i + 1 for i in range(len(nums)) if nums[i] > 0]
 
 # Plus One
+'''
 
+Given a non-negative int represented as a non-empty array of digits, plus one
+to the integer.
+
+Digits are sorted such that the most significant digit is at the head of the list
+
+
+'''
 # Max Consecutive Ones
 
 # Shortest Unsorted Continuous Subarray
@@ -1041,6 +1536,56 @@ Array
 # best time to buy and sell stock Ii
 
 # Best time to Buy and Sell Stock
+'''
+
+Need to find out the maximum difference between two numbers in the given arrays
+second number, selling price, must be larger than the first one, buying price
+
+https://leetcode.com/articles/best-time-buy-and-sell-stock/
+
+Approach #1 (Brute Force) [Time Limit Exceeded]
+
+for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit)
+                    maxprofit = profit;
+
+Approach #2 (One Pass) [Accepted]
+. We need to find the largest peak following the smallest valley. We can maintain
+two variables - minprice and maxprofit corresponding to the smallest valley and
+ maximum profit (maximum difference between selling price and minprice) obtained
+  so far respectively.
+
+TC: O(n)
+SC: O(1)
+'''
+
+def maxProfit(prices):
+    low = float('inf')
+    profit = 0
+    for i in prices:
+        profit = max(profit, i-low)
+        low = min(low, i)
+    return profit
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices or len(prices) == 1:
+            return 0
+        min_price = prices[0]
+        max_profit = 0
+        for num in prices:
+            if num < min_price:
+                min_price = num
+            elif num - min_price > max_profit:
+                max_profit = num - min_price
+        return max_profit
+
 
 # Reshape the matrix
 
@@ -1049,6 +1594,40 @@ Array
 # Pascal's Triangle Ii
 
 # Pascal's Triangle
+'''
+Given numRows, generate the first numRows of Pascal's triangle
+
+For exameple, given numRows = 5
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+'''
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if not numRows:
+            return []
+
+        if numRows == 1:
+            return [[1]]
+
+        result = [[1]]
+        for i in range(1, numRows):
+            result.append([1])
+            for j in range(1, i):
+                result[i].append(result[i - 1][j - 1] + result[i - 1][j])
+            result[i].append(1)
+
+        return result
+
 
 # Maximum Product of three Numbers
 
@@ -1057,3 +1636,68 @@ Array
 # Contains Duplciate Ii
 
 # Merge Sorted Array
+
+
+
+
+
+# Serach a 2D matrix
+
+
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if matrix == None or len(matrix) == 0 or len(matrix[0]) == 0: return False
+        m, n = len(matrix), len(matrix[0])
+        lo, hi = 0, m * n - 1
+        while lo <= hi:
+            mid = (lo + hi) / 2
+            x = mid / n
+            y = mid % n
+            if matrix[x][y] == target:
+                return True
+            elif matrix[x][y] > target:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+
+        return False
+
+
+
+# Stair Search - O(n)
+'''
+# start from the top right,
+if greater than matrix[0][columnLength-1], go down
+
+
+'''
+
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        n = len(matrix)
+        if n == 0: return False
+        m = len(matrix[0])
+        if m == 0: return False
+
+        i, j = 0, m - 1
+        while i < n and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif  target > matrix[i][j]:
+                i += 1
+            else: # target < matrix[i][j]
+                j -= 1
+
+        return False
