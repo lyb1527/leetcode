a17d43d2926666d893dc9b65e42511df5b71a588
lyb1527 <yingbo_liu@brown.edu>
2017-07-05 23:05:12 -0400

    more leetcode questions

--- leetcode.py
+++ leetcode.py
@@ -1923,6 +1923,33 @@ class Solution(object):
                     dp[j] = 0
         return dp[n - 1]
 
+
+# Minimum Path Sum
+"""
+Given m*n grid filled with non-negative numbers, find a path from top left to
+bottom right which mimimizes the sum of all numbers
+
+NOTE: move one down or right
+"""
+
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m = len(grid)
+        n = len(grid[0])
+        for i in range(1, m):
+            grid[i][0] += grid[i - 1][0]
+        for j in range(1, n):
+            grid[0][j] += grid[0][j - 1]
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+        return grid[-1][-1]
+
+
 # 3sum
 '''
 Given an array S of n integers, find all unique triplets that gives the sum of zero
